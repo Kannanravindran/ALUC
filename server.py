c24e0864f39376d23edaaaa87c0cddb36a510d22
ethorse <snipertech@protonmail.com>
2017-10-29 18:45:08 -0700

    latest cleanup

--- server.py
+++ server.py
@@ -10,11 +10,16 @@ def index():
 
 @app.route('/groupify-similar',methods=['get'])
 def groupify():
-    return render_template("groupified.html",groups=exec_similar(),scores=scores)
+    return render_template("groupified.html",groups=exec_similar(),scores=[1,2,3,4,5,6,7,8,9,10])
 
 @app.route('/groupify-dissimilar',methods=['get'])
 def groupify_similar():
     return render_template("groupified.html", groups=exec_dissimilar(),scores=scores)
+
+@app.route('/collab',methods=['get'])
+def collab():
+    return render_template("collab.html")
+
 
 if __name__ == '__main__':
     app.run()
---
+++ static/app.js
@@ -0,0 +1,3 @@
+var doc = document.getElementById('doc1');
+doc.contentEditable = true;
+doc.focus();
